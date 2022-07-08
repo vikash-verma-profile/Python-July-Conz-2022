283f1345172322f0698dbf9d46f6d4aea6b15b02
Level-Up-4 <vikash-verma-profile>
2022-07-08 09:58:19 +0530

    first commit

---
+++ sample1.py
@@ -0,0 +1,25 @@
+print("Hello")
+
+#this is a sample comment
+
+x=5
+y="Vikash"
+
+print(x)
+print(y)
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+q='sample'
+#both are same
+p="sample"
+
+a=4
+A="Vikash Verma"
+print(a)
+print(A)
