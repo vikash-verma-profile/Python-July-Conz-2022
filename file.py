4d67a3e6a80d94be6a2e3acef4f7a728d2c3fa4b
Level-Up-4 <vikash-verma-profile>
2022-07-08 13:01:34 +0530

    added python file handling

---
+++ file.py
@@ -0,0 +1,15 @@
+# f=open("files/dummy.txt","r")
+# print(f.read())
+
+# f=open("files/dummy.txt","a")
+# f.write("Hi My name is vikash Verma");
+# f.close();
+
+# f=open("files/dummy.txt","r")
+# print(f.read())
+# f.close();
+# import os
+# os.remove("files/dummy.txt")
+
+import os
+os.rmdir("files")
